f6335afd79a099efea41191e2917f51a36405d3e
Woitekku <wojciech@cichy.org>
2024-02-07 17:58:24 +0100

    ohho

--- src/hello-world-nodejs/server.js
+++ src/hello-world-nodejs/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! oh!');
+  res.send('Hello World! ohho!');
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
