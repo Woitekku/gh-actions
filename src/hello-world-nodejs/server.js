9f8cde43c8fb4806a4d6d69f8d6ae7a26f494423
Woitekku <wojciech@cichy.org>
2024-02-07 17:17:49 +0100

    tri

--- src/hello-world-nodejs/server.js
+++ src/hello-world-nodejs/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Two Cognito');
+  res.send('Hello World! Tri Cognito');
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
