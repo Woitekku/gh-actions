5e880cb53678c9b3bf03e3e0cefb0c42542bb441
Woitekku <wojciech@cichy.org>
2024-02-07 14:42:30 +0100

    dynamic destroy

--- infrastructure/_modules/ecs/r53.tf
+++ infrastructure/_modules/ecs/r53.tf
@@ -8,3 +8,14 @@ resource "aws_route53_record" "this" {
       evaluate_target_health = true
     }
 }
+
+resource "aws_route53_record" "dynamic" {
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = format("*.%s", var.domain_name)
+  type    = "A"
+  alias {
+      name = aws_lb.this[0].dns_name
+      zone_id = aws_lb.this[0].zone_id
+      evaluate_target_health = true
+    }
+}
